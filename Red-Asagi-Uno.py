9c16b33e24a8b7a082ff806721fba0bb25a43f13
FPC <felipepalmacastro@gmail.com>
2022-05-19 18:34:30 -0400

    Update Red-Asagi-Uno.py

--- Red-Asagi-Uno.py
+++ Red-Asagi-Uno.py
@@ -4,11 +4,11 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+## proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 # Metralleta
 for x in range(1,100):
-    r = requests.get('http://localhost:8081/api/v1/bikes', verify=False, proxies=proxies)
+    r = requests.get('192.168.1.102:8081/api/v1/bikes', verify=False)
 
 print("Red Asagi One\n")
 
