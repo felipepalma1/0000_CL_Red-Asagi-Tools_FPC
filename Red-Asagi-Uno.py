dcc61b14d832f74754c72feedd6abb434659b482
FPC <felipepalmacastro@gmail.com>
2022-05-19 18:37:12 -0400

    Update Red-Asagi-Uno.py

--- Red-Asagi-Uno.py
+++ Red-Asagi-Uno.py
@@ -8,7 +8,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Metralleta
 for x in range(1,100):
-    r = requests.get('192.168.1.102:8081/api/v1/bikes', verify=False)
+    r = requests.get('http://192.168.1.102:8081/api/v1/bikes', verify=False)
 
 print("Red Asagi One\n")
 
